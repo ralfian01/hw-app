bc84a5602c17f33adef5738a5726766553d08128
ANGEOM21 <idrisanjounghan313@gmail.com>
2024-05-27 20:24:58 +0700

    landing page web

--- FRONTEND/mobile/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { authReducer } from "@features/auth";
-import { configureStore } from "@reduxjs/toolkit";
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer
-  }
-});
